649f39eaf91b4482c45fee9274a58ed93a84687d
AlexandrShumkov <alshumkov2@yandex.ru>
2022-01-29 15:20:30 +0300

    Task added

---
+++ Task/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число от 1 до 1000: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if ((a % 7 == 0) && (a % 23 == 0))
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Некратно");
+}
